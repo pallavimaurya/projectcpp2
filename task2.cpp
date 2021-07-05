9261768f948f508199bce08cf3e9c4cd8dc9356b
pallavimaurya <86961873+pallavimaurya@users.noreply.github.com>
2021-07-05 17:49:17 +0530

    Create task2.cpp
    This file prints the product of two numbers on screen.

---
+++ task2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int product,a,b;
+  cout<<"enter the two number";
+  cin>>a>>b;
+  
+  product=a*b;
+  cout<<"product="<<product;
+  return 0;
+}
+  
